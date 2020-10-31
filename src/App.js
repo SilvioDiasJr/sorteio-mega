5e08dfd67a0dc6d0355777d3abe67c70abfb336d
Silvio <silvio.dias@etec.sp.gov.br>
2020-10-30 21:50:44 -0300

    adicionar componente Sorteio

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Sorteio from './Sorteio'
+
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <Sorteio />
+    </div>
+  );
+}
+
+export default App;
